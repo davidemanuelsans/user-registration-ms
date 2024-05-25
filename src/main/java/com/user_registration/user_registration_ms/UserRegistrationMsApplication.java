c934546427dcb976c288863340e6ea6b8382f919
davidemanuelsans <davidemanuelsans@gmail.com>
2024-05-25 11:40:12 -0500

    Initial commit

---
+++ src/main/java/com/user_registration/user_registration_ms/UserRegistrationMsApplication.java
@@ -0,0 +1,13 @@
+package com.user_registration.user_registration_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user_registration/user_registration_ms/UserRegistrationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_registration.user_registration_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
